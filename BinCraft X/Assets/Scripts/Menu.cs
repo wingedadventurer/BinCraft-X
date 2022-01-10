8af62076a00fddf8c6b66a291568751eb427d91f
Karlo Koščal <wingedadventurer@gmail.com>
2022-01-10 13:38:14 +0100

    implement async load of game scene

--- BinCraft X/Assets/Scripts/Menu.cs
+++ BinCraft X/Assets/Scripts/Menu.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject panelMain, panelLoading;
+    public Slider slider;
+
+    private void Start()
+    {
+        panelMain.SetActive(true);
+        panelLoading.SetActive(false);
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        StartCoroutine(LoadGameScene());
     }
 
     public void QuitGame()
     {
         Debug.Log("quit");
         Application.Quit();
+    }
+
+    private IEnumerator LoadGameScene()
+    {
+        panelMain.SetActive(false);
+        panelLoading.SetActive(true);
+        slider.value = 0;
+
+        AsyncOperation load = SceneManager.LoadSceneAsync(1);
+
+        while (!load.isDone)
+        {
+            slider.value = load.progress;
+            yield return null;
+        }
     }
 }
